9daab664218cccb69dc611059460f8c73923bbec
NicolasGS02 <nicolasgonsan2004@gmail.com>
2026-05-10 22:41:44 +0200

    Añadimos favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'BermellonShop') }}</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 
     @vite(['resources/js/app.js', 'resources/css/app.scss'])
