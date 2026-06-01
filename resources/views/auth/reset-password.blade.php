29e6edd32f6473582597f79a7c5118c1074adf52
NicolasGS02 <nicolasgonsan2004@gmail.com>
2026-06-01 18:50:06 +0200

    Arreglo para despliegue en el servidor

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -11,11 +11,16 @@
                 </div>
 
                 <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            {{ __('app.reset_password_error') }}
+                        </div>
+                    @endif
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
-                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-                        <input type="hidden" name="email" value="{{ $request->email }}">
+                        <input type="hidden" name="token" value="{{ old('token', $request->route('token')) }}">
+                        <input type="hidden" name="email" value="{{ old('email', $request->email) }}">
 
                         <div class="mb-4 text-sm text-muted">
                             {{ __('app.reset_password_email') }} <strong>{{ $request->email }}</strong>
